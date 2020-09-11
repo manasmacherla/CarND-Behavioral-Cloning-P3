1f76905ef46c1ba1ac379ec5a7ecc6f930390fbd
Manas Macherla <rmacher@g.clemson.edu>
2020-09-10 22:58:33 -0400

    stupid mistake in choosing kernel but everything works until the bridge!!

--- clone.py
+++ clone.py
@@ -9,6 +9,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import MSE
 
 lines = []
+
 #opening the csv file with the file names and measurements 
 with open('C:/Users/manas/OneDrive - Clemson University/Documents/SDC_github/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -29,13 +30,37 @@ for line in lines:
     measurements.append(measurement)
     measurements.append(-measurement)
 
+
+
+"""
+with open('C:/Users/manas/OneDrive - Clemson University/Documents/SDC_github/driving_log.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        steering_center = float(row[3])
+
+        # create adjusted steering measurements for the side camera images
+        correction = 0.2 # this is a parameter to tune
+        steering_left = steering_center + correction
+        steering_right = steering_center - correction
+
+        # read in images from center, left and right cameras
+        path = "C:/Users/manas/OneDrive - Clemson University/Documents/SDC_github/IMG" # fill in the path to your training IMG directory
+        img_center = process_image(np.asarray(Image.open(path + row[0])))
+        img_left = process_image(np.asarray(Image.open(path + row[1])))
+        img_right = process_image(np.asarray(Image.open(path + row[2])))
+
+        # add images and angles to data set
+        car_images.extend(img_center, img_left, img_right)
+        steering_angles.extend(steering_center, steering_left, steering_right)
+"""
+
 X_train = np.array(images)
 Y_train = np.array(measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160,320,3)))
-model.add(tf.keras.layers.Conv2D(6, (34,68), (2,4), padding="valid", activation='relu'))
-model.add(tf.keras.layers.Conv2D(12, (2,2), (2,2), padding="valid", activation='relu'))
+model.add(tf.keras.layers.Conv2D(6, 5, (2,4), padding="valid", activation='relu'))
+model.add(tf.keras.layers.Conv2D(12, 5, (2,2), padding="valid", activation='relu'))
 model.add(tf.keras.layers.Conv2D(12, 5, 1, padding="valid", activation='relu')) #shape = 28*28*12
 model.add(tf.keras.layers.MaxPooling2D(2,2,padding='valid')) #shape = 14*14*12
 model.add(tf.keras.layers.Conv2D(16, 5, 1, padding="valid", activation='relu')) #shape = 10*10*16
